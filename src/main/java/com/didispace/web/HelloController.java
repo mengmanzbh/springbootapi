9dda244406f7c4df8ed84f7b9f1fc84b8f8edfed
mengmanzbh <1361006944@qq.com>
2019-01-06 23:03:55 +0800

    ok

--- src/main/java/com/didispace/web/HelloController.java
+++ src/main/java/com/didispace/web/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.JSONObject;
+import net.sf.json.JSONObject;
 
 @RestController
 public class HelloController {
