8e335ac1287137dbdfbd4f4ac69f49172abe3339
mengmanzbh <1361006944@qq.com>
2019-01-06 22:25:13 +0800

    ok

--- src/main/java/com/didispace/web/HelloController.java
+++ src/main/java/com/didispace/web/HelloController.java
@@ -10,5 +10,21 @@ public class HelloController {
     public String index() {
         return "Hello World";
     }
+ 
+@ResponseBody
+@RequestMapping(value = "/moblie", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+public String writeByBody(@RequestBody JSONObject jsonParam) {
+    // 直接将json信息打印出来
+    System.out.println(jsonParam.toJSONString());
+
+    // 将获取的json数据封装一层，然后在给返回
+    JSONObject result = new JSONObject();
+    result.put("msg", "ok");
+    result.put("method", "@ResponseBody");
+    result.put("data", jsonParam);
+
+    return result.toJSONString();
+}
+
 
 }
